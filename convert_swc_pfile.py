ba89b5cab5514c01d07485421e873518fdfdc77e
jreed20 <jreed20@bupkis.krasnow.gmu.edu>
2019-02-14 16:28:39 -0500

    Modifies .swc file format to .p file format to run morphology files in simulation

---
+++ convert_swc_pfile.py
@@ -0,0 +1,68 @@
+'''<Reformats .swc data file format into .p file format.>
+    Copyright (C) <2019> <Jonathan Reed>
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
+#Usage: python convert_swc_pfile.py --file dSPN-Sham-A1S1N1.CNG.swc
+#Output: dSPN-Sham-A1S1N1.CNGconvert.p
+
+#George Mason University
+#Jonathan Reed
+#February 14, 2019
+
+import argparse
+import datetime
+parser = argparse.ArgumentParser()
+parser.add_argument("--file")
+args = parser.parse_args()
+filename = args.file
+
+copy_lines = []; newlines = []; parlist = []
+lines = open(filename, 'r').readlines()
+
+#copies intended values for .pfile from .swcfile into temp. list
+for line in lines:
+    if line[0] !='*' and line[0] !='/' and line[0] != '\n' and line[0] != '\r' and line[0] != '#':
+        copy_line = line.split()
+        copy_lines.append(copy_line)
+
+#list of parent values
+for x in range(5):
+    parlist.append(list(zip(*copy_lines))[x])
+
+out_name = filename.split('.swc')[0] + 'convert.p'
+outfile = open(out_name,'w')
+outfile.write('*Original .swc file : ')
+outfile.write(filename + '\n')
+outfile.write('*Modified to .p file on : ')
+outfile.write(str(datetime.datetime.now()) + '\n')
+outfile.write('\n')
+
+#rearranges values for .p format
+for num,line in enumerate(copy_lines):
+    newline = line[:6]
+    newline[0] = (str(line[0]) + '_' + str(line[1]))
+    if num == 0: #soma line always first in .swc file, unique parent-type 'none'
+        newline[1] = 'none'
+    else:
+        parent_index = parlist[0].index(line[6])  #parlist[0] refers to parent location of comp at line[6]
+        parent_type = parlist[1][parent_index]
+        for x in range(2,5):
+            newline[x] = round(float(copy_lines[num][x]) - float(parlist[x][parent_index]),4)
+        newline[1] = str(line[6]) + '_' + str(parent_type)
+    newline[5] = round(float(newline[5])*2,4) #.p file format takes diameter values instead of radius
+    write_line = [str(val) for val in newline]
+    write_line = ' '.join(write_line) #converts from list of strings to single string
+    outfile.write(write_line + '\n')
+
+print('Converted ' + str(len(copy_lines)) + ' compartments from original file : ' + str(filename))
+print('Modified file created : ' + str(out_name))
+outfile.close()
