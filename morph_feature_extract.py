d4f2490d086ce8f6f7725126cf29ec399a5e119c
Jonathan Reed <jreed@nebish.cen.krasnow.gmu.edu>
2021-02-14 19:00:27 -0500

    Updated comments for clear usage.

--- morph_feature_extract.py
+++ morph_feature_extract.py
@@ -1,5 +1,5 @@
 '''<Extracts data values from .swc files>
-    Copyright (C) <2020>  <Jonathan Reed>
+    Copyright (C) <2021>  <Jonathan Reed>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,26 +20,30 @@
 
 #George Mason University
 #Jonathan Reed
-#September 8, 2020
+#February 14, 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
 import btmorph
-
 import argparse
 import datetime
 import os
 import glob
 
-'''Copy end compartment and branching compartment data for continuing compartments'''
+'''Recursive function to calculate path distance features for all dendritic compartments in morphology'''
+#In morphology, continuing compartments span between soma compartment, branch points, and terminal ends
+#Calculates feature values of terminal ends/branch points to proximal branch points/soma, and continuing compartments in-between
+
 def find_paths(swc_tree, node_data, stats, local_list): 
-    for item in local_list:                              #will complete the endpoint(s) for subsequent node for each end/branch point sent to function
+    for item in local_list:   #search for selected terminal end/branch point; stops at proximal branch point or soma
         if item[INDEX] == node_data[COMP].parent.index and item[INDEX] not in soma and item[COMP] not in stats._bif_points:
             comp_len = stats.get_pathlength_to_root(node_data[COMP]) - stats.get_pathlength_to_root(node_data[COMP].parent)
             new_length = node_data[BRANCH_LEN] + comp_len
-            new_endpath = node_data[PATH_TO_END] + comp_len 
+            new_endpath = node_data[PATH_TO_END] + comp_len
             item.extend((node_data[END_POINT],node_data[BRANCH_POINT],new_length,new_endpath))
-            local_list = find_paths(swc_tree, item, stats, local_list)     #recursive function to calculate all continuning compartments towards upstream branch point
+            #save end point, branch point, total dendritic length at node, longest single end path at node
+            local_list = find_paths(swc_tree, item, stats, local_list)
+            #recursively calculate path distances of continuing compartments by adding compartment length 
     return local_list                                                    
 
 '''Flatten nested lists of values into single list of values'''
@@ -59,12 +63,12 @@ path = args.path
 if not os.path.exists(path):
     print('Error in Path')
 else:
-    path = path + '*CNG.swc'
+    path = path + '*CNG.swc' #must have 'CNG.swc' as end of swc file name
     
     '''Load .swc morphologies from folder'''
     for filename in glob.glob(path):
         soma = []; morph_list = []; local_list = []; degree_list = []
-        #soma compartments, feature list, feature calculation values, compartment order from terminals toward soma (Node Degree)
+        #soma compartments, feature list, feature calculations, compartment terminal branch order (Node Degree)
 
         #Load morphology with btmorph
         swc_tree = btmorph.STree2()                         
@@ -79,47 +83,54 @@ else:
             if node.get_content()['p3d'].type == 1:              #separate soma compartment(s) from dendritic compartments
                 soma.append(node.index)                          #soma compartments designated Type = 1 from .swc file format
         degree_list.sort(key=lambda x: x)
-        local_list.sort(key=lambda x: x[DEGREE])                 #sorted list by node degree
+        local_list.sort(key=lambda x: x[DEGREE])                 #sorted list by Node Degree
 
         '''Calculate Feature Values associated with Path Distances'''
-        #Start calculations from terminal branches with node degree = 1
+        #Start calculations from terminal branches with Node Degree = 1
         for item in stats._end_points:
             self_node = [c for c in local_list if c[COMP] == item]
             self_node[0].extend(([self_node[0][INDEX]],[0],0,0))
-            #end points, branch points, total dendritic length, longest path to terminal end 
-            local_list = find_paths(swc_tree, self_node[0], stats, local_list)     #calculate compartments from terminal ends to distal branch points
-
-        #Start calculations from branch compartments and continuing (in between) compartments; traversing towards soma
+            #end points, branch points, initiate path distances at terminal ends as 0
+            local_list = find_paths(swc_tree, self_node[0], stats, local_list)     
+            #terminal ends and terminal branches (Node Degree = 1) calculated
+            
+        #Start calculations for remaining branch points and continuing compartments towards soma
+        #For remaining compartments (Node Degree > 1) path distance features require:
+        #Sum of multiple paths (total dendritic length)
+        #Comparison of branching pathes for single longest path  
         for num in degree_list[1:]:
-            for item in stats._bif_points:                                            #branch/bifurcation compartments
+            for item in stats._bif_points:   #branch/bifurcation compartments
                 if item.index not in soma and stats.degree_of_node(item) == num:
                     self_node = [c for c in local_list if c[COMP] == item]
-                    child_1 = [c for c in local_list if c[COMP] == item.children[0]]  #find children (subsequent) compartments of branch compartments
-                    child_2 = [c for c in local_list if c[COMP] == item.children[1]] 
-
-                    end_1 = child_1[0][END_POINT]
-                    end_2 = child_2[0][END_POINT]
+                    
+                    #find children (subsequent) compartments of branch compartments
+                    child_1 = [c for c in local_list if c[COMP] == item.children[0]]  
+                    child_2 = [c for c in local_list if c[COMP] == item.children[1]]
+                    
+                    #find all endpoints which stem from branch compartments
+                    end_1 = child_1[0][END_POINT]; end_2 = child_2[0][END_POINT]
                     self_node[0].append([end_1, end_2])
-                    self_node[0][END_POINT] = list(flatten(self_node[0][END_POINT]))        #find all endpoints which stem from branch compartments
+                    self_node[0][END_POINT] = list(flatten(self_node[0][END_POINT]))        
 
-                    branch_1 = child_1[0][BRANCH_POINT]
-                    branch_2 = child_2[0][BRANCH_POINT]
+                    #find all branchpoints which stem from branch compartments 
+                    branch_1 = child_1[0][BRANCH_POINT]; branch_2 = child_2[0][BRANCH_POINT]
                     self_node[0].append([branch_1,branch_2,self_node[0][INDEX]]) 
-                    self_node[0][BRANCH_POINT] = list(flatten(self_node[0][BRANCH_POINT]))  #find all branchpoints which stem from branch compartments 
+                    self_node[0][BRANCH_POINT] = list(flatten(self_node[0][BRANCH_POINT]))  
 
-                    child_1_path = child_1[0][BRANCH_LEN]
-                    child_2_path = child_2[0][BRANCH_LEN]
+                    #calculate the total dendritic length rooted at branch compartment
+                    child_1_path = child_1[0][BRANCH_LEN]; child_2_path = child_2[0][BRANCH_LEN]
                     branch_path_1 = child_1_path + (stats.get_pathlength_to_root(child_1[0][COMP]) - stats.get_pathlength_to_root(self_node[0][COMP]))
                     branch_path_2 = child_2_path + (stats.get_pathlength_to_root(child_2[0][COMP]) - stats.get_pathlength_to_root(self_node[0][COMP]))
                     branch_len = branch_path_1 + branch_path_2
-                    self_node[0].append(branch_len)                         #calculate the total dendritic length rooted at branch compartment
+                    self_node[0].append(branch_len)                         
 
+                    #calculate the longest path to terminal end rooted at branch compartment
                     end_path_1 = child_1[0][PATH_TO_END] + (stats.get_pathlength_to_root(child_1[0][COMP]) - stats.get_pathlength_to_root(self_node[0][COMP]))
                     end_path_2 = child_2[0][PATH_TO_END] + (stats.get_pathlength_to_root(child_2[0][COMP]) - stats.get_pathlength_to_root(self_node[0][COMP]))
                     path = end_path_1 if end_path_1 > end_path_2 else end_path_2
-                    self_node[0].append(path)                               #calculate the longest path to terminal end rooted at branch compartment
+                    self_node[0].append(path)                               
 
-                    #calculate feature values by adding compartment length for compartments in-between branch compartments
+                    #calculate feature values by adding compartment length for continuing compartments in-between branch compartments
                     local_list = find_paths(swc_tree, self_node[0], stats, local_list)
         
         '''Organize all Feature Values by Compartment'''
@@ -148,6 +159,7 @@ else:
                 else:
                     temp.append(2)                                         #'2' - Continuing (Remaining Compartments)
                 morph_list.append(temp)
+                #if additional features for morphology, include in temp list and add label below
 
         '''Save to file'''
         dirname = os.path.dirname(filename)
@@ -160,7 +172,7 @@ else:
         outfile.write('*Extracted .swc data on : ')
         outfile.write(str(datetime.datetime.now()) + '\n')
         outfile.write('\n')
-        outfile.write('*CHILD; TYPE; X; Y; Z; RADIUS; NODE_DEGREE; NODE_ORDER; PARENT; PARENT_RAD; PATH_DIS; HS; BRANCH_LEN; PATH_TO_END; PAR_CONNECT')
+        outfile.write('*CHILD; TYPE; X; Y; Z; RADIUS; NODE_DEGREE; NODE_ORDER; PARENT; PARENT_RAD; PATH_DIS; HS; BRANCH_LEN; PATH_TO_END; PAR_CONNECT') #if additional features, add to label
         outfile.write('\n')
 
         for line in morph_list:
